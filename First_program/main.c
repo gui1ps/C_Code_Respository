8fe3b467175bca83ca0959998a6740536064a9c1
Guilherme Roberto dos Passos <guilhermerobertodospassos@gmail.com>
2023-08-11 15:28:17 -0300

    Hello, world! :)

---
+++ First_program/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/*
+int main(){
+    int age=10;
+    printf("Hello, world!%d\n",age);
+    printf("Second print");
+    return 0;
+}
+*/
+/*
+int main(){
+    float val1=4;
+    float val2=5;
+    float val3=6;
+    int qnt=3;
+    printf("Media: %f",(val1+val2+val3)/qnt);
+    return 0;
+}
+*/
+/*
+int main(){
+    float sal=3420.65;
+    float desconto=0.10;
+    printf("Total com desconto: %f",sal-(sal*desconto));
+    return 0;
+}
+*/
+/*
+int main(){
+    char name[3];
+    float sal;
+    scanf("%f",&sal);
+    gets(name);
+    return 0;
+}*/
+/*
+int main(){
+    printf("Você deseja iniciar? [s/n]: ");
+    char resp=getchar();
+    return 0;
+}*/
+/*
+int main() {
+    int x;
+    double num;
+    char text[20];
+    scanf("%d %lf %s", &x, &num, text);
+    printf("%d %lf %s", x, num, text);
+}*/
+int main(){
+    printf("\"HELLO MUNDOO!\"\n");
+    printf("\"HELLO MUNDOO!\"\t TAAB");
+    return 0;
+}
