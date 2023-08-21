11e75c12cd1681cc484ab530cbc416dd91e9943e
Guilherme Roberto dos Passos <guilhermerobertodospassos@gmail.com>
2023-08-21 08:40:30 -0300

    Adding programming exercises

---
+++ College/Estruturas condicionais/4/matematica.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("De input em dois numeros e escolha uma opção para realizar a operação\n");
+    char resp;
+    float num1,num2;
+
+    do{
+       printf("[x] SAIR | [+] SOMAR |  [-] SUBTRAIR |  [*] MUTIPLICAR | [/] DIVIDIR\n");
+       resp=getchar();
+       printf("Numero: ");
+       scanf("%f",&num1);
+       printf("Numero: ");
+       scanf("%f",&num2);
+
+       switch (resp)
+       {
+        case 'x':
+            printf("SAINDO ...\n");
+            break;
+        case '+':
+            printf("A soma entre %f e %f: %f\n",num1,num2,(num1+num2));
+            break;
+        case '-':
+            printf("A subtracao entre %f e %f: %f\n",num1,num2,(num1-num2));
+            break;
+        case '*':
+            printf("A multiplicacao entre %f e %f: %f\n",num1,num2,(num1*num2));
+            break;
+        case '/':
+            printf("A divisao entre %f e %f: %f\n",num1,num2,(num1/num2));
+            break;
+        default:
+            printf("COMANDO INVÁLIDO");
+            break;
+       }
+
+    }while (resp!='x');
+    
+    return 0;
+}
